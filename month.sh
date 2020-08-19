4bd1d00db1e8aee252881905e9f92739e10a10c6
msandhyareddy <sandhyareddy72@gmail.com>
2020-08-20 00:03:35 +0530

    month.sh

---
+++ month.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+year=`date +%y`
+month=`date +%m`
+day=`date +%d`
+hour=`date +%H`
+minute=`date +%M`
+second=`date +%S`
+echo 'date'
+echo "the current dateis: $day-$month-$year"
+echo "the current time is: $hour:$minute:$second"
